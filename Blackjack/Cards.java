b87eef34bf03209de561db58968a213f9973fa68
Pawel <49004622+Pawi2323@users.noreply.github.com>
2022-02-03 12:15:40 +0100

    malo sie zmienilo

---
+++ Blackjack/Cards.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+class Cards{
+    private void Cards(){
+        String cards[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+        Random r = new Random();
+        int rand = r.nextInt(13);
+        String drawnCard = cards[rand];
+    }
+}
